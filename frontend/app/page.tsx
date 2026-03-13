3d879cb66f7aec66d828a93f6f094eeb80053605
Nicolás Mora <nmora2805@gmail.com>
2026-03-13 16:40:17 -0500

    style(PLN-138): ajuste codigo base en la pagina princial

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,9 @@
+import React from 'react'
 
-
-export default function Home() {
+function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-5xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-7xl">
-          Bienvenido a Planify
-        </h1>
-        <p className="mt-4 text-2xl text-gray-500 dark:text-gray-400">
-          Sistema de administracion de horarios.
-        </p>
-      </main>
-    </div>
-  );
+    <div>HomePage</div>
+  )
 }
+
+export default HomePage
