74be4ddf6796400a98ee807c233dae1ed175e4de
havo-ao <ao7@hotmail.es>
2026-03-27 17:48:19 -0500

    feat(PLN-155): teachers ui implemented

---
+++ frontend/src/jsx/layouts/Footer.jsx
@@ -0,0 +1,20 @@
+ import React from "react";
+
+const Footer = () => {
+  var d = new Date();
+  return (
+    <div className="footer">
+      <div className="copyright">
+        <p>
+          Copyright © Designed &amp; Developed by{" "}
+          <a href="https://github.com/nmorab28/planify-monorepo" target="_blank" rel="noreferrer">
+            Planify
+          </a>{" "}
+          {d.getFullYear()}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
